3dcaceddb1302378f90191a2b1b3d68c5adaf922
Alex Kuzevich <kuzewich.a@gmail.ru>
2020-02-04 21:49:27 +0300

    tests

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * The entire MVC needs a database to work properly
+ */
+
+self::$config['database'] = array(
+    'hostname' => 'localhost',
+    'username' => 'user_wallet',
+    'password' => 'i3F6oDIHYvuPC3r2',
+    'dbname' => 'payments'
+);
+
+self::$config['salt'] = "dlkcmd22_fv(4&*4mvnKDNHHsAP)_)kdldi12cdl::L847GTe]|sdc*74";
+
+?>
