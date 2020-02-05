2458f77fedd16c060dc63b860076d57902f8da7a
Alex Kuzevich <kuzewich.a@gmail.ru>
2020-02-05 13:07:39 +0300

    new deploy change

--- deploy.pl
+++ deploy.pl
@@ -4,4 +4,5 @@ echo '';
 cd /var/www/domains/m.wallets.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
 git pull https://github.com/notStonks/phpRepository.git > /dev/null
+chmod -R 755 /var/www/domains/m.wallets.qzo.su
 #git@bitbucket.org:gafurovstudio/g2.git
