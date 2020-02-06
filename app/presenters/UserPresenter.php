a6c6fd60391a160d4a546909e894e49558319adc
Alex Kuzevich <kuzewich.a@gmail.ru>
2020-02-06 15:06:08 +0300

    deleted args from getUserInfo

--- app/presenters/UserPresenter.php
+++ app/presenters/UserPresenter.php
@@ -5,7 +5,7 @@ class UserPresenter extends MainPresenter {
 	public static $isSecurity = false;
 
 	public function getListUsers(){ echo (new Users())->getListUsers(); }
-	public function getUserInfo($id){echo (new Users())->getUserInfo($id);}
+	public function getUserInfo(){echo (new Users())->getUserInfo();}
 }
 
 ?>
