220ba0fb37b311ff830f14757288f353331fedda
Alex Kuzevich <kuzewich.a@gmail.com>
2020-06-05 15:17:39 +0300

    fix

--- app/presenters/CurrenciesPresenter.php
+++ app/presenters/CurrenciesPresenter.php
@@ -7,7 +7,6 @@ class CurrenciesPresenter extends MainPresenter {
     public function getListCurrencies(){ echo (new Currencies())->getListCurrencies(); }
     public function getCurrencyInfo(){echo(new Currencies())->getCurrencyInfo();}
     public function addCurrency(){echo(new Currencies())->addCurrency();}
-    //public function addCurrency1(){echo(new Currencies())->addCurrency1();}
     public function editCurrencyStatus(){echo(new Currencies())->editCurrencyStatus();}
     public function editCurrency(){echo(new Currencies())->editCurrency();}
 
