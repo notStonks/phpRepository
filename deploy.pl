d6bf58e807485e426818b129ccd72f61afd84805
Alex Kuzevich <kuzewich.a@gmail.com>
2020-06-18 21:30:27 +0300

    upd

--- deploy.pl
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Content-type: text/plain\n";
-echo '';
-cd /war/www/server/wallets.su/ || exit > /dev/null
-git reset --hard > /dev/null
-git pull https://github.com/notStonks/phpRepository.git > /dev/null
-chmod -R 755 /war/www/server/wallets.su
-#git@bitbucket.org:gafurovstudio/g2.git
